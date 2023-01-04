12f69f0bd15fca0ee752a221d43653d91b5228db
UNAI ALFARO GARCIA <unai.alfaro@ikasle.aeg.eus>
2023-01-04 23:00:33 +0100

    Terminado falta el popup

--- next.config.js
+++ next.config.js
@@ -11,5 +11,8 @@ module.exports = {
   },
   images:{
     domains:['upload.wikimedia.org','a0.muscache.com','links.papareact.com']
+  },
+  env:{
+    mapbox_key: 'pk.eyJ1IjoidW5haWlpIiwiYSI6ImNsY2h3N2h3ZDA0bTYzb3A2cmV1cTQ0eXYifQ.AaIj4oYDp0v43Zfi4grn-A'
   }
 }
--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,12 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import InfoCard from "../components/InfoCard";
+import Map from "../components/Map";
 import { useRouter } from 'next/router';
 import { format } from "date-fns";
 
 function Search({searchResults}){
     const router = useRouter();
-
-    console.log(searchResults);
 
     const {location, startDate, endDate, noOfGuests} = router.query;
     const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
@@ -45,6 +44,9 @@ function Search({searchResults}){
                             />
                         ))}
                     </div>
+                </section>
+                <section className="hidden xl:inline-flex xl:min-w-[600px]">
+                    <Map searchResults={searchResults}/>
                 </section>
             </main>
             <Footer/>
