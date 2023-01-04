a2ef35dd6309e3e01a7985157e772681ad29f389
UNAI ALFARO GARCIA <unai.alfaro@ikasle.aeg.eus>
2023-01-04 03:35:15 +0100

    Primera pagina hecha

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import {MagnifyingGlassIcon,GlobeAltIcon,UsersIcon,UserCircleIcon,Bars3Icon} fro
 
 function Header (){
     return(
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md shadow-gray-300 p-5 md:px-10">
              <div className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image 
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/768px-Airbnb_Logo_B%C3%A9lo.svg.png"
